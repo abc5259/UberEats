b63943e6ac87649c7a7cb411a736eacf1c651aa1
LeeJaeHoon <dlwogns3413@naver.com>
2022-05-03 13:30:14 +0900

    feat: add Verification Entity and verifyEmail Mutation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,11 +6,15 @@ import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { JwtService } from 'src/jwt/jwt.service';
 import { EditProfileInput } from './dtos/edit-profile.dto';
+import { Verification } from './entities/verification.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User) private readonly users: Repository<User>,
+    @InjectRepository(User)
+    private readonly users: Repository<User>,
+    @InjectRepository(Verification)
+    private readonly verifications: Repository<Verification>,
     private readonly jwtService: JwtService,
   ) {}
 
@@ -24,10 +28,13 @@ export class UsersService {
       if (exists) {
         return { ok: false, error: '이미 존재하는 이메일 입니다.' };
       }
-      await this.users.save(this.users.create({ email, password, role }));
+      const user = await this.users.save(
+        this.users.create({ email, password, role }),
+      );
+      await this.verifications.save(this.verifications.create({ user }));
       return { ok: true };
     } catch (e) {
-      return { ok: false, error: '계정울 생성할 수 없습니다.' };
+      return { ok: false, error: '계정을 생성할 수 없습니다.' };
     }
   }
 
@@ -36,7 +43,10 @@ export class UsersService {
     password,
   }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
     try {
-      const user = await this.users.findOne({ where: { email } });
+      const user = await this.users.findOne({
+        where: { email },
+        select: ['id', 'password'],
+      });
       if (!user) {
         return {
           ok: false,
@@ -71,10 +81,28 @@ export class UsersService {
     const user = await this.users.findOne({ where: { id } });
     if (email) {
       user.email = email;
+      user.verified = false;
+      await this.verifications.save(this.verifications.create({ user }));
     }
     if (password) {
       user.password = password;
     }
     return this.users.save(user);
+  }
+
+  async verifyEmail(code: string): Promise<boolean> {
+    try {
+      const verification = await this.verifications.findOne({
+        where: { code },
+        relations: ['user'],
+      });
+      if (!verification) throw new Error();
+      verification.user.verified = true;
+      this.users.save(verification.user);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 }
