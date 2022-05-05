feac3e4650df7d505b768a25d4755d7f73570c85
LeeJaeHoon <dlwogns3413@naver.com>
2022-05-05 19:14:08 +0900

    feat: createAccount E2E TEST 완료

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { getConnection } from 'typeorm';
+import got from 'got';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +25,65 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
+  });
+
+  describe('createAccount', () => {
+    const EMAIL = 'dlwogns3413@gmail.com';
+    it('User을 만들어야한다.', async () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "${EMAIL}"
+              password: "1234"
+              role: Client
+            }) {
+              ok 
+              error 
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('이메일이 이미 있다면 실패해야한다.', async () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "${EMAIL}"
+              password: "1234"
+              role: Client
+            }) {
+              ok 
+              error 
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
   it.todo('createAccount');
+  it.todo('userProfile');
   it.todo('login');
   it.todo('me');
-  it.todo('userProfile');
-  it.todo('editProfile');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
